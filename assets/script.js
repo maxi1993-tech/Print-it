918dbbeb176447c70b2b8d253ab7c3ced1bdd666
maxi1993-tech <maxi1993.cm@gmail.com>
2026-07-14 19:50:09 +0200

    style: fix indentation in script.js

--- assets/script.js
+++ assets/script.js
@@ -21,30 +21,29 @@ const arrowLeft = document.querySelector(".arrow_left");
 const arrowRight = document.querySelector(".arrow_right");
 let currentIndex = 0;
 const bannerImg = document.querySelector(".banner-img");
-const bannerText = document.querySelector("#banner p")
+const bannerText = document.querySelector("#banner p");
 
 arrowLeft.addEventListener("click", function() {
 	if (currentIndex === 0) {
-    currentIndex = slides.length - 1
-    } else { 
-      currentIndex = currentIndex - 1
-      
-    }
+		currentIndex = slides.length - 1;
+	} else {
+		currentIndex = currentIndex - 1;
+	}
 	document.querySelector(".dot_selected").classList.remove("dot_selected");
 	document.querySelectorAll(".dot")[currentIndex].classList.add("dot_selected");
-    bannerImg.src = "./assets/images/slideshow/" + slides[currentIndex].image;
+	bannerImg.src = "./assets/images/slideshow/" + slides[currentIndex].image;
 	bannerText.innerHTML = slides[currentIndex].tagLine;
 });
 
 arrowRight.addEventListener("click", function() {
 	if (currentIndex === slides.length - 1) {
-      currentIndex = 0;         
-    } else { 
-      currentIndex = currentIndex + 1
-    }
+		currentIndex = 0;
+	} else {
+		currentIndex = currentIndex + 1;
+	}
 	document.querySelector(".dot_selected").classList.remove("dot_selected");
 	document.querySelectorAll(".dot")[currentIndex].classList.add("dot_selected");
-    bannerImg.src = "./assets/images/slideshow/" + slides[currentIndex].image;
+	bannerImg.src = "./assets/images/slideshow/" + slides[currentIndex].image;
 	bannerText.innerHTML = slides[currentIndex].tagLine;
 });
 
